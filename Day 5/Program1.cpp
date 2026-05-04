28f046c8d66eb230dab8f78047ef98c281a7531c
Partshah2905 <parthshahh2905@gmail.com>
2026-05-04 20:12:31 +0530

    Day5Program1

---
+++ Day 5/Program1.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int main() {
+    int rows = 5;    // Height
+    int cols = 15;   // Width
+
+    // Nested loops to print the rectangle
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << "*";
+        }
+        cout << endl; // Move to next line
+    }
+
+    return 0;
+}
