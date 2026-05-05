0e7dd4596a1765696359fab8bf38240fdbdc4018
Partshah2905 <parthshahh2905@gmail.com>
2026-05-05 11:38:22 +0530

    Day6Program6

---
+++ Day 5/Program6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Function using a reference (&) to update the original variable
+void applyDiscount(double &bill) {
+    double discount = bill * 0.10;
+    bill = bill - discount; // Original bill is updated directly
+}
+
+int main() {
+    double bill;
+
+    // Get input from user
+    cout << "Enter the original bill amount: ";
+    cin >> bill;
+
+    applyDiscount(bill);
+
+    cout << "Bill after 10% discount: " << billAmount << endl;
+
+    return 0;
+}
