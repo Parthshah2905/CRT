5daaaef2dce6bf524ae1bc885ec0f78f4f1fe3cf
Partshah2905 <parthshahh2905@gmail.com>
2026-05-09 10:14:17 +0530

    Day8Program2

---
+++ Day 8/Program1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[3] [3];
+    for {inti=0;i<3;i++}{
+        for {intj=0;j<3:j++}{
+            cout<<arr[i][j]<" ";
+        }
+    }
+
+    int arr[3] [3];
+    for {inti=0;i<3;i++}{
+        for {intj=0;j<3:j++}{
+            cout<<arr[i][j]<" ";
+        }
+    }
+
+
+    // int size = sizeof{arr} / sizeof{arr(0)};
+
+   //  for {int i=0 ; i<size ; i++}
+   //  {
+   //     cin>>arr{i};
+   //  }
+   //  for{int i=0;i<size;i++}
+   //  {
+   //      cout<<arr{i}<<" ";
+   // }
+    return 0;
+}
