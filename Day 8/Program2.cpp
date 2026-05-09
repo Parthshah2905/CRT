dbfeb0a7c5f359ff6f4c934a791ba634b3862213
Partshah2905 <parthshahh2905@gmail.com>
2026-05-09 11:23:54 +0530

    Day8program2

---
+++ Day 8/Program2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+class sum;
+int sumfArray{int num[], int n}
+{
+    int sum=0;
+    for {int i=0;i<n;i++{
+        sum+=nums[i];
+    }
+    return sum;
+   }
+}
